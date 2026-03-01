cd9139cb64fe38074a3a6a36a6c61e044055602c
BagusSedana <gedebagussedanayoga28@gmail.com>
2026-03-01 23:43:34 +0800

    Reorder LocalSEO section to appear before Stats marquee

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -26,8 +26,8 @@ export default function App() {
       <Navbar />
       <Hero />
       <LogoMarquee />
-      <Stats />
       <LocalSEO />
+      <Stats />
       <Services />
       <TextMarquee />
       <Portfolio />
