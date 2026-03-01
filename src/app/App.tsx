45a8a094702c249168bb5a4e0b8f0c70077ec3b1
BagusSedana <gedebagussedanayoga28@gmail.com>
2026-03-01 23:38:04 +0800

    Reorder LocalSEO section to appear before Services section

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -27,9 +27,9 @@ export default function App() {
       <Hero />
       <LogoMarquee />
       <Stats />
+      <LocalSEO />
       <Services />
       <TextMarquee />
-      <LocalSEO />
       <Portfolio />
       <WhyUs />
       <Process />
